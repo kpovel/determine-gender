1d496b7779fa7949143973c75d5acb4c6ef5d8ee
kpovel <kar.pavel123@gmail.com>
2022-04-02 01:21:49 +0300

    add error handling

--- main.js
+++ main.js
@@ -4,16 +4,25 @@ const result = document.getElementById('result')
 const serverUrl = 'https://api.genderize.io'
 
 button.addEventListener('click', sendRequest)
-input.addEventListener("keydown", function (e){
-    if (e.code === 'Enter'){
+input.addEventListener("keydown", function (e) {
+    if (e.code === 'Enter') {
         sendRequest()
     }
 })
 
-function sendRequest (){
+function sendRequest() {
     const firstName = input.value
     const url = `${serverUrl}?name=${firstName}`
     fetch(url)
         .then(response => response.json())
-        .then(commits => result.textContent =`${firstName} - ${commits.gender}`)
+        .then(function (commits) {
+            try {
+                if (commits.gender === null || commits.gender === undefined) {
+                    throw new Error('unknown gender')
+                }
+                result.textContent = `${firstName} - ${commits.gender}`
+            } catch (e) {
+                result.textContent = `${firstName} - ${e.message}`
+            }
+        })
 }
